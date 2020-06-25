e992bf37b6bed646999fbbc0a64de989b6c30de6
Sharmas <sharmas.sharru@gmail.com>
2020-06-25 16:44:56 +0530

    Maven Project 01 - first commit

---
+++ src/test/java/testmaven/testmaven/AppTest.java
@@ -0,0 +1,34 @@
+package testmaven.testmaven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit; 
+import org.openqa.selenium.By;
+
+@Test
+public class AppTest {
+WebDriver driver;
+
+
+@Test
+public void test()
+{
+	System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Chrome Driver\\chromedriver_win32\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.get("https://www.myntra.com/");
+	driver.manage().window().maximize(); 
+	System.out.println(driver.getTitle());
+	driver.close();
+//	driver.findElement(By.className("image-image undefined image-hand")).click();
+//	driver.findElement(By.className("desktop-linkButton")).click();
+ //   driver.findElement(By.className("desktop-input")).sendKeys("Jack"); 
+//	driver.findElement(By.className("seerch-icon")).click(); 
+//	driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS); 
+}
+
+
+
+}
